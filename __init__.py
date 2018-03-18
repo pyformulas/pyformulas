6a6b665657360439025ff083f15eef32df37a5cb
pyformulas <pyformulas@gmail.com>
2018-03-18 14:38:10 -0600

    Change structure

--- __init__.py
+++ __init__.py
@@ -2,14 +2,14 @@ class Class:
     pass
 
 def screen(canvas=None, title='Display'):
-    from pyformulas._screen import screen
+    from _formulas._screen import screen
     globals()['screen'] = screen
 
     return screen(canvas, title)
 
 audio = Class()
 def _play(wavedata, bitrate=None, duration=None, block=False):
-    from pyformulas._audio._play import play
+    from _formulas._audio._play import play
     globals()['audio'].play = play
 
     return play(wavedata, bitrate, duration, block)
@@ -17,7 +17,7 @@ audio.play = _play
 
 
 def download(url, out_path=None, get_headers=False, get_body=None):
-    from pyformulas._download import download
+    from _formulas._download import download
     globals()['download'] = download
 
     return download(url, out_path, get_headers, get_body)
