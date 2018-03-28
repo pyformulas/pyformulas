299d8b824d1ae5fe6a33c64bc0cb76b7526d2ae3
pyformulas <pyformulas@gmail.com>
2018-03-27 20:59:23 -0600

    Edit TCP_stream.py

--- examples/TCP_stream.py
+++ examples/TCP_stream.py
@@ -8,4 +8,7 @@ class play_chess(pf.net.Stream):
         text = buffer.decode('utf-8', 'ignore')
         print(text)
 
+        if 'Message Of The Day' in text:
+            self.disconnect()
+
 play_chess(port=23, address='freechess.org')
